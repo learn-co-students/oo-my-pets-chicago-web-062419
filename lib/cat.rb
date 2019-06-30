d3a5f7b9e6b7db25fe035663f8f3e34b46c729db
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-06-30 15:21:34 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,29 @@
 class Cat
   # code goes here
+  attr_reader :name
+  attr_accessor :mood, :owner
+
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.count
+    self.all.count
+  end
+
 end
+
+# cat_one = Cat.new("maru")
+# p cat_one.name
+# p cat_one.mood
+# cat_one.mood = "angry"
+# p cat_one.mood
